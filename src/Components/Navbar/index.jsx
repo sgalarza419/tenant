2aa5305e85455dd2bda9cdc13e5860d0b5513049
Matthew <smith0223@ymail.com>
2021-01-13 13:26:43 -0500

    updates

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./style.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
