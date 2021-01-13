643a9f80ad67b6841442c54baee72693e20b8136
S Galarza <sgalarza4190@gmail.com>
2021-01-13 10:41:24 -0500

    added component folder and files

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '../Button';
+
+function Card() {
+    return(
+        <div>
+            This is a Card!
+            <Button />
+        </div>
+    )
+}
+
+export default Card;
