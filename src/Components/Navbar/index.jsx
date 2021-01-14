7a8790a9b61dc1e5352486818d59ac9d9af2703d
Dimitar Micevski <dmicevski@hotmail.com>
2021-01-14 10:59:25 -0500

    updated brand name in nav bar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-       Paradise
+       CDMS Connect
       </Link>
       <div>
         <ul className="navbar-nav">
