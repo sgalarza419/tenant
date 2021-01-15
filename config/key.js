e7186cdeb3164f0f404f8244ce2123503fda3102
Matthew <smith0223@ymail.com>
2021-01-15 12:52:12 -0500

    hoping this fixes the files

---
+++ config/key.js
@@ -0,0 +1,12 @@
+module.exports={
+    google:{
+        clientID: '691701312112-gom2qvokg8cjmfun59bpcklhlcug6be5.apps.googleusercontent.com',
+        clientSecret: 'ui8DnQvjDHxWvD8oGHnfnVEb'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://sgalarza:aries419@cluster0.yquit.mongodb.net/tenant?retryWrites=true&w=majority'
+    },
+    session: {
+        cookieKey: 'weloveresidents'
+    }
+}
