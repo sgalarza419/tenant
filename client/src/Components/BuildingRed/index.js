7087bf5d76e4ebc805aacc42a93a9b7e94db1aa0
Crozarts <imarcrosby@optimum.net>
2021-01-18 19:46:56 -0500

    tenant page changes

---
+++ client/src/Components/BuildingRed/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+function BuildingRed() {
+    return (
+        // add on click function to button for action
+        <button className="buttonRed"><img src="images/building_red.png"/></button>
+    )
+}
+
+export default BuildingRed;
--- client/src/Pages/Tenant.jsx
+++ client/src/Pages/Tenant.jsx
@@ -1,42 +1,22 @@
 import React from "react";
-import TenantCard from "../Components/Card/Tenant"
+// import TenantCard from "../Components/Card/Tenant"
 import Jumbotron from "react-bootstrap/Jumbotron"
 import Row from "../Components/Row"
-// import Building from '../Components/Building'
+import BuildingRed from '../Components/BuildingRed'
 // import Container from "../Components/Container"
 
 function Tenant() {
   return (
     <div>
-      <container style={{ paddingTop: 30 }}>
-        {/* <Row>
-          <Col size="md-12"> */}
-          {/* commment */}
-            <Jumbotron>
-              <br></br>
-            <h1 style={{textAlign:"center", color:"teal"}}>Welcome Tenant!</h1> 
-            </Jumbotron>
-            <h2 style={{textAlign:"center", color:"teal"}}>Here are your Current Rentals...</h2>
-            <Row>
-            <TenantCard />
-            </Row>
-            <p>
-              Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-              tortor, facilisis in nque sit amet, scelerisque elementum tortor. Nullam eget nibh
-              sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
-              congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
-              vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
-              Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
-              libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
-              eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
-              interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
-              massa.
-            </p>
- 
-          {/* </Col>
-        </Row> */}
+      <container style={{ paddingTop:"30px" }}>
+        <Jumbotron style={{backgroundImage: "https://media.istockphoto.com/photos/estate-agent-giving-house-keys-to-woman-and-sign-agreement-in-office-picture-id1130829500?k=6&m=1130829500&s=612x612&w=0&h=MHR5K8GvILOjSKOLKdgPeav1DPpQPqEbxEr1FTrEpiU=", backgroundSize: "cover"}}>
+          <br></br>
+          <h1 style={{ textAlign: "center", color: "teal" }}>Welcome Tenant!</h1>
+        </Jumbotron>
+        <h2 style={{ textAlign: "center", color: "teal" }}>Here are your Current Rentals...</h2>
+        <Row>
+          <BuildingRed />
+        </Row>
       </container>
     </div>
   );
