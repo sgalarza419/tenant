849b7ed3a33db11385101e822e8dc091b891c1f1
S Galarza <sgalarza4190@gmail.com>
2021-01-18 20:38:26 -0500

    working on login page

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TenantCard from "../Components/Card/Tenant"
 import Jumbotron from 'react-bootstrap/Jumbotron'
 // import Container from "../Components/Container"
+import Card from "react-bootstrap/Card"
 
 function Login() {
   return (
@@ -9,21 +10,31 @@ function Login() {
       {/* <Container style={{ marginTop: 30 }}> */}
         {/* <Row>
           <Col size="md-12"> */}
-            <Jumbotron> 
-            <h1 className="text-center">Use your Gmail account to sign up</h1> 
+          <Jumbotron style={{
+              display:"flex",
+              justifyContent:"center",
+              alignItem:"center"
+              }}> 
+            <h1 className="text-center" style={{
+              float:"left",
+              color: "teal",
+              fontSize: "50px"
+              }}>Use your Gmail account to sign up Today!</h1> 
+            </Jumbotron>
             <main style={{ 
-              maxWidth: "960px",
+                maxWidth: "960px",
                 margin: "30px auto",
                 padding: "0 10px",
-                color: "#333" }}>
+                color: "#333",
+                textAlign: "center"}}>
             <a class="google-btn" href="/auth/google" style={{
                 color: "#fff",
                 textDecoration: "none",
                 fontSize: "18px",
                 padding: "10px",
-                background: "#ff5353" }}>Gmail</a>
+                background: "#ff5353",
+                }}>Gmail</a>
             </main>
-            </Jumbotron>
           {/* </Col>
         </Row> */}
       {/* </Container> */}
