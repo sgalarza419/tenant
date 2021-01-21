57ef6ccdfa2eb690dcd9ab177fa4a1d00d2ab231
Crozarts <imarcrosby@optimum.net>
2021-01-20 19:30:25 -0500

    added dummy payment page, card to tenant for more fluid design

--- client/src/Pages/Payment.jsx
+++ client/src/Pages/Payment.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
-
+import Row from 'react-bootstrap/row';
+import Container from '../Components/Container';
+import Col from '../Components/Col';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCcStripe } from '@fortawesome/free-brands-svg-icons';
 
 function Payment() {
     return (
         <div>
+            <Container style={{ marginTop: "50px" }}>
+                <Row>
+                    <h2 style={{ color: "teal" }}>Here is your current bill</h2>
+                </Row>
+                <Row></Row>
+                <Col size="md-2">
+                    <h3 style={{ color: "teal" }}>Bill Amount: </h3>
+                </Col>
+                <Col size="md-2">
+                    <h3>$985.96</h3>
+                </Col>
+                <Row>
+                    <Col size="md-2">
+                        <h4 style={{ fontSize: "16px" }}>Name on card</h4>
+                        <input className="creditCard" type="form" />
+                    </Col>
+                    <Col size="md-2">
+                        <h4 style={{ fontSize: "16px" }}>Credit card number</h4>
+                        <input className="creditCard" type="form" />
+                    </Col>
+                    <Col size="md-2">
+                        <h4 style={{ alignItem: "center", fontSize: "16px" }}>Date</h4>
+                        <input className="month" style={{ width: "30px" }} type="form" />
+                        <input className="day" style={{ width: "20px" }} type="form" />
+                        <input className="year" style={{ width: "35px" }} type="form" />
+                    </Col>
+                    <Col size="md-2">
+                        <h4 style={{ alignItem: "center", fontSize: "16px" }}>CVC</h4>
+                        <input style={{ width: "35px" }} type="form" />
+                    </Col>
+                </Row>
+                <br></br>
+                <Row>
+                        <Button variant="info">Send Payment</Button>
+                </Row>
+                <br></br>
+                <h6>or...</h6>
+                <Row style={{marginTop:"10px"}}>
+                        <Button href="https://stripe.com/?utm_campaign=paid_brand-US_Search_Brand_Stripe-1803852691&utm_medium=cpc&utm_source=google&ad_content=448938759825&utm_term=stripe&utm_matchtype=e&utm_adposition=&utm_device=c&gclid=CjwKCAiAxp-ABhALEiwAXm6IyY9Q2-FyUYFLot1WJeeWOfRxNUivZUIAWAUNIluSrZyhIEqTbH1gLBoC4JgQAvD_BwE" variant="info" style={{ width: "100px" }}><FontAwesomeIcon icon={faCcStripe} size="3x" color="white" /></Button>
+                        <h5>sign up using stripe</h5>
+                </Row>
+            </Container>
         </div>
     )
 }
--- client/src/Pages/Tenant.jsx
+++ client/src/Pages/Tenant.jsx
@@ -4,8 +4,11 @@ import Row from "../Components/Row";
 import Button from 'react-bootstrap/Button';
 import Modal from '../Components/Modal';
 import Card from 'react-bootstrap/Card';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComments } from "@fortawesome/free-solid-svg-icons";
+import { faMoneyBillWave } from "@fortawesome/free-solid-svg-icons";
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-
 
 // Needs function to populate rental data
 
@@ -16,12 +19,67 @@ function Tenant() {
     <div>
       <container style={{ paddingTop: "30px" }}>
         <Hero backgroundImage="images/TenantHero.jpg"></Hero>
-
+        <br></br>
         <h1 style={{ fontSize: "100px", textAlign: "center", color: "teal" }}>Welcome Tenant!</h1>
         <h4 style={{ textAlign: "center", color: "teal" }}>Message Landlords, Pay Bills, and Rent SMARTER with Tenant.</h4>
-          <br></br>
+        <Row>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front text-white">
+                {/* <Image src={Img5} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} fluid/> */}
+                <h5 style={{ marginTop: "140px" }}><FontAwesomeIcon icon={faMoneyBillWave} size="5x" color="white" /></h5>
+                <h4>Make Payments</h4>
+              </div>
+              <div className="flip-card-back text-white">
+                <br />
+                <br />
+                <br />
+                <h1>Make Payments</h1>
+                <p>Convenient way for landlords to get paid. Rent, utilities, move-in fees and more are deposited straight into your bank account — at no cost to you.</p>
+                <FontAwesomeIcon icon={faMoneyBillWave} size="5x" color="white" />
+              </div>
+            </div>
+          </div>
+
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front text-white">
+                {/* <Image src={Img5} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} fluid/> */}
+                <h5 style={{ marginTop: "140px" }}><FontAwesomeIcon icon={faComments} size="5x" color="white" /></h5>
+                <h4>Chat with Landlord/Tenants</h4>
+              </div>
+              <div className="flip-card-back text-white">
+                <br />
+                <br />
+                <br />
+                <h1>Chat with Landlord/Tenants</h1>
+                <p>Communication is seamless!! Landlords can keep in contact with their tenants and tenants can chat with other tenants!!</p>
+                <FontAwesomeIcon icon={faComments} size="5x" color="white" />
+              </div>
+            </div>
+          </div>
+
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front text-white">
+                {/* <Image src={Img5} alt="Avatar" style={{ width: "400px", height: "400px", marginTop: "40px" }} fluid/> */}
+                <h5 style={{ marginTop: "140px" }}><FontAwesomeIcon icon={faUserPlus} size="5x" color="white" /></h5>
+                <h4>Sign Up</h4>
+              </div>
+              <div className="flip-card-back text-white">
+                <br />
+                <br />
+                <br />
+                <h1>Sign Up</h1>
+                <p>Registering is a breeze and you'll be up and running in no time!! Just click on the icon below!!</p>
+                <FontAwesomeIcon icon={faUserPlus} size="5x" color="white" />
+              </div>
+            </div>
+          </div>
+        </Row>
+        <br></br>
         <h2 style={{ textAlign: "center", color: "teal" }}>Here are your Current Rentals...</h2>
-        
+
         <Row>
           {/* Button for car pop up populated with rental information */}
           <Button variant="info" onClick={() => setIsOpen(true)}>Click Here!</Button>
@@ -29,17 +87,17 @@ function Tenant() {
 
         <Modal open={isOpen} onClose={() => setIsOpen(false)}>
 
-          <Card style={{ width: '500px', backgroundColor: "teal"}}>
+          <Card style={{ width: '500px', backgroundColor: "teal" }}>
             <Card.Body>
-              <Card.Title style={{ textAlign: "center", color: "white"}}>Here is your current Rentals</Card.Title>
-                <Card.Text style={{ textAlign: "center", color: "white"}}>
-                  populated data goes here
+              <Card.Title style={{ textAlign: "center", color: "white" }}>Here is your current Rentals</Card.Title>
+              <Card.Text style={{ textAlign: "center", color: "white" }}>
+                populated data goes here
                 </Card.Text>
-                <Row>
+              <Row>
                 <Button variant="info" href="/payment">Pay Bills</Button>
                 <Button variant="info" href="/chat">Open Discussion</Button>
                 <Button variant="info">Manage Rentals</Button>
-                </Row>
+              </Row>
             </Card.Body>
           </Card>
 
